c720c01f65b00ce74f1f7298a7f9922fec2af086
tjkr0wn <joumaa_family@icloud.com>
2021-07-07 00:47:36 -0400

    Moved all testing stuff to development branch

--- tests/read_blockdev.c
+++
@@ -1,8 +0,0 @@
-asm(".space 0x620, 0x0\n\t");
-asm(".text\n");
-extern long init(void);
-
-long read_blockdev(void) {
-  long image_info = init();
-  return image_info;
-}
--- tests/tools/reloc_load_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#jank city
-bin=$(readlink -f ../../main_test.bin.final)
-echo $bin
-mv ./load_bin.py $1
-python $1/load_bin.py $bin
-mv $1/load_bin.py ./
